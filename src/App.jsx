1d06faf9c43deb6c44cf1f490f97bbd0e5ffcb8b
dianaboikoo <eaa23dibo@students.eaaa.dk>
2024-11-11 11:11:01 +0100

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AccountPage from './pages/AccountPage';
